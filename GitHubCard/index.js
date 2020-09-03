ed226454a9a299a5634ef16817645918f57b8770
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-03 17:34:09 -0500

    Did I forget to make commits? ....yes, yes I did. BUt anyways, MVP is finished!

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
@@ -17,6 +18,21 @@
     and append the returned markup to the DOM as a child of .cards
 */
 
+const cards = document.querySelector('.cards')
+
+axios.get('https://api.github.com/users/fatima-rizvi')
+  .then(stuff => {
+    console.log('the data: ',stuff.data)
+    const myCard = cardMaker(stuff.data);
+    console.log(myCard);
+    cards.appendChild(myCard)
+    // debugger;
+  })
+  .catch(err => {
+    console.log(err);
+  })
+// const myCard = cardMarker(stuff.data);
+
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,7 +44,27 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['Jahteo','fibonacci','robertmasters','chasingeuphoria', 'kubes2020'];
+
+followersArray.forEach(followerName => {
+  let mainURL = 'https://api.github.com/users/'
+  let followerURL = mainURL.concat(followerName)
+  axios.get(followerURL)
+  .then(stuff => {
+    console.log('the data: ',stuff.data)
+    const myCard = cardMaker(stuff.data);
+    console.log(myCard);
+    cards.appendChild(myCard)
+    // debugger;
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
+
+
+
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +85,53 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(obj) {
+  //Instantiating elements
+  const card = document.createElement('div')
+  const userImg = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const nameOfUser = document.createElement('h3')
+  const userName = document.createElement('p')
+  const userLocation = document.createElement('p')
+  const profile = document.createElement('p')
+  const githubAddress = document.createElement('a')
+  const followersNum = document.createElement('p')
+  const followingNum = document.createElement('p')
+  const userBio = document.createElement('p')
+  //Setting class names, attributes and text
+  //attributes and names
+  userImg.src = obj.avatar_url
+  nameOfUser.textContent = `${obj.name}`
+  userName.textContent = `${obj.login}`
+  userLocation.textContent = `Location: ${obj.location}`
+  profile.textContent = 'Profile:'
+  githubAddress.href = obj.html_url
+  githubAddress.textContent = `${obj.html_url}`
+  followersNum.textContent = `Followers: ${obj.followers}`
+  followingNum.textContent = `Following : ${obj.following}`
+  userBio.textContent = `Bio: ${obj.bio}`
+  //class names
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  nameOfUser.classList.add('name')
+  userName.classList.add('username')
+  //creating the heirarchy
+  //start with card
+  card.appendChild(userImg)
+  card.appendChild(cardInfo)
+  //Move onto card info
+  cardInfo.appendChild(nameOfUser)
+  cardInfo.appendChild(userName)
+  cardInfo.appendChild(userLocation)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(followersNum)
+  cardInfo.appendChild(followingNum)
+  //Lastly, profile
+  profile.appendChild(githubAddress)
+
+  return card
+}
 
 /*
   List of LS Instructors Github username's:
